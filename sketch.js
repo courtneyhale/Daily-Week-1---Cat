7f427977e7e5f9ecb960020324dbd27623ac43ec
courtneyhale <courtneyhale44@gmail.com>
2016-01-28 20:32:33 -0500

    Cat Daily Week 1

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(900,900);
+  fill('orange');// orange cat
+  triangle(250,100,400,200,280,300);//left ear
+  triangle(750,100,600,200,720,300);//right ear
+  ellipse(500,400,500,500);//cat face
+  fill('black');//black nose
+  ellipse(500,500,60,60);//cat nose
+  fill('white');//white eye
+  ellipse(400,350,60,80);//left eye
+  ellipse(600,350,60,80);//right eye
+  fill('black');//black eyeball
+  ellipse(400,365,30,50);//left eyeball
+  ellipse(600,365,30,50);//right eyeball
+  line(350,450,500,500);//whisker
+  line(350,500,500,500);//whisker
+  line(360,550,500,500);//whisker
+  line(400,500,640,500);//whisker
+  line(500,500,640,450);//whisker
+  line(500,500,640,550)
+}
+
+function draw() {
+  
+}
